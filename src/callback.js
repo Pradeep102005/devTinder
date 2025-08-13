38ce4cd76160634b51d9614f11ca6884d636ab01
Pradeep Palakodeti <pradeeppalakodeti24@gmail.com>
2025-08-13 15:16:25 +0530

    multi-routehandlers

--- src/callback.js
+++ src/callback.js
@@ -1,10 +1,20 @@
 const express=require('express');
 const app=express();
+app.use("/user",(req,res,next)=>{
+    //route handler 1
+    console.log("Handline the route user!!");
+    //   res.send("Route handler1");
+      next();
+},
+(req,res)=>{
+    //route handler 2
+    console.log("Handling the route user 2!!")
+    res.send("2nd response!!");
+}
+);
 
-app.get('/user/:userId/:name/:password',(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Pradeep",lastName:"Palakodeti"});
-});
+
+
 app.listen(7777,()=>{
 console.log("Server started running 7777");
 })
